b90487a86d89329608f33d8d00e27487b194ac69
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-09 20:59:41 -0800

    Add header effect on scroll

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
 import '../styles/header.css'
 
 export default function Header() {
+  const [isBlurred, setIsBlurred] = useState(false);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    setIsBlurred(scrollY > 1); // If greater than 1, set to blur, otherwise don't blur
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
+  
   return (
-    <div className="header">
+    <div className={isBlurred ? 'header blurred' : 'header'}>
       <div className="contact-fields">
         <div className="name">Josh Sawyer</div>
         <a className="github" href="https://github.com/joshusooyaa"><img src="../../public/github-mark-white.svg" alt="github" /></a>
