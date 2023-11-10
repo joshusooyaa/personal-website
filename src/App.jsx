67db34fc3bf1c61cd4b71ee563981f5e3c38b12b
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-09 16:15:05 -0800

    Setup React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/header'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="about">About</div>
+      <div className="projects">Projects</div>
+      <div className="contact">Contact</div>
+      <div className="resume">Resume</div>
+    </div>
+  )
+
+}
