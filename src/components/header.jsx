9dd42ae54fe904dd27b01261acc66d11248d4051
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-09 16:53:37 -0800

    Add styling to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,14 @@ import '../styles/header.css'
 
 export default function Header() {
   return (
+    <>
     <div className="header">
       <div className="about">About</div>
       <div className="projects">Projects</div>
       <div className="contact">Contact</div>
       <div className="resume">Resume</div>
     </div>
+    </>
   )
 
 }
