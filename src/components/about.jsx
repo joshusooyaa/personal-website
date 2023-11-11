01db586ac242a131c15f9c437de9cd6d1874b933
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-10 18:37:24 -0800

    Add base about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,16 @@
+import SectionHeader from './section-header'
+
 import '../styles/about.css'
 
 export default function About() {
   return (
     <div className="about-section">
-      about section
+      <SectionHeader title="About" />
+      <div className="about-container">
+        <p className="section intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p className="section interests">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p className="section skills">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
     </div>
   )
 }
---
+++ src/components/section-header.jsx
@@ -0,0 +1,10 @@
+import '../styles/section-header.css'
+
+export default function SectionHeader( {title} ) {
+
+  return (
+    <>
+      <h3 className="section-header">{title}</h3>
+    </>
+  )
+}
