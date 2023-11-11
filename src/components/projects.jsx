88570289f0179d421ee56c87bbf39861dd4bddc7
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-10 19:39:23 -0800

    Edit project base style

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,24 +2,35 @@ import SectionHeader from './section-header'
 
 import '../styles/projects.css'
 
+function Project() {
+  return (
+    <div className="projects-container">
+          <div className="project1 wrapper">
+            <div className="display"></div>
+            <div className="information">
+              <p>Description here</p>
+              <div className="links">
+              <a className="github" href="https://github.com/joshusooyaa"><img src="../../public/github-mark-white.svg" alt="github" /></a>
+              <a className="live-preview" href="https://joshusooyaa.github.io/etch-a-sketch/"><img src="../../public/live-preview.png" alt="live preview" /></a>
+              </div>
+            </div>
+            <div className="technologies">
+              Tech used...
+            </div>
+          </div>
+    </div>
+  )
+}
+
+
 export default function Projects() {
   return (
     <div className="projects-section">
       <SectionHeader title="Projects"/>
-      <div className="projects-container">
-        <div className="project1 wrapper">
-          <div className="display"></div>
-          <div className="information">
-            <p>Description here</p>
-            <div className="links">
-              a
-            </div>
-          </div>
-          <div className="technologies">
-            Tech used...
-          </div>
-        </div>
-      </div>
+      <Project />
+      <Project />
+      <Project />
+  
     </div>
   )
 }
