df21fc30219127bf554293ea9001ee361cf4aa7d
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-10 19:28:11 -0800

    Build base project wrapper

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,9 +1,25 @@
+import SectionHeader from './section-header'
+
 import '../styles/projects.css'
 
 export default function Projects() {
   return (
     <div className="projects-section">
-      projects section
+      <SectionHeader title="Projects"/>
+      <div className="projects-container">
+        <div className="project1 wrapper">
+          <div className="display"></div>
+          <div className="information">
+            <p>Description here</p>
+            <div className="links">
+              a
+            </div>
+          </div>
+          <div className="technologies">
+            Tech used...
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
