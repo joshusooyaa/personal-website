811f8d24817ed6fed0f778489a266315e4fe9435
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-12 17:03:48 -0800

    Update project section
    Not finished. Layout still needs to be built. Previous layout has
    been removed and replaced with the start of this one for a better
    look.

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,35 +4,43 @@ import '../styles/projects.css'
 
 import etchProjectImage from '../assets/etch-a-sketch.png'
 
+// function Project( {image, description } ) {
+//   return (
+//         <div className="project1 wrapper">
+//           <div className="display" style={{backgroundImage: `url(${image}`}}></div>
+//           <div className="information">
+//             <p>{description}</p>
+//             <div className="links">
+//             <a className="github" href="https://github.com/joshusooyaa"><img src="../../public/github-mark-white.svg" alt="github" /></a>
+//             <a className="live-preview" href="https://joshusooyaa.github.io/etch-a-sketch/"><img src="../../public/live-preview.png" alt="live preview" /></a>
+//             </div>
+//           </div>
+//           <div className="technologies">
+//             Tech used...
+//           </div>
+//         </div>
+//   )
+// }
+
 function Project( {image, description } ) {
   return (
-    <div className="projects-container">
-          <div className="project1 wrapper">
-            <div className="display" style={{backgroundImage: `url(${image}`}}></div>
-            <div className="information">
-              <p>{description}</p>
-              <div className="links">
-              <a className="github" href="https://github.com/joshusooyaa"><img src="../../public/github-mark-white.svg" alt="github" /></a>
-              <a className="live-preview" href="https://joshusooyaa.github.io/etch-a-sketch/"><img src="../../public/live-preview.png" alt="live preview" /></a>
-              </div>
-            </div>
-            <div className="technologies">
-              Tech used...
-            </div>
-          </div>
+    <div className="project">
+      <img className="project-image" src={image} alt="project image" />
+      <div className="information">
+        <p>{description}</p>
+      </div>
     </div>
   )
 }
-
 
 export default function Projects() {
   
   return (
     <div className="projects-section">
       <SectionHeader title="Projects"/>
-      <Project image={etchProjectImage} description="Description Here"/>
-      <Project />
-      <Project />
+      <div className="projects-container">
+        <Project image={etchProjectImage} description="Hi there"/>
+      </div>
     </div>
   )
 }
