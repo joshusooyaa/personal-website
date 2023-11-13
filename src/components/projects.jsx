0fa6089a347ff0dfd313432e2dcc423b863c96df
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-12 18:41:22 -0800

    Update project layout

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,30 +4,19 @@ import '../styles/projects.css'
 
 import etchProjectImage from '../assets/etch-a-sketch.png'
 
-// function Project( {image, description } ) {
-//   return (
-//         <div className="project1 wrapper">
-//           <div className="display" style={{backgroundImage: `url(${image}`}}></div>
-//           <div className="information">
-//             <p>{description}</p>
-//             <div className="links">
-//             <a className="github" href="https://github.com/joshusooyaa"><img src="../../public/github-mark-white.svg" alt="github" /></a>
-//             <a className="live-preview" href="https://joshusooyaa.github.io/etch-a-sketch/"><img src="../../public/live-preview.png" alt="live preview" /></a>
-//             </div>
-//           </div>
-//           <div className="technologies">
-//             Tech used...
-//           </div>
-//         </div>
-//   )
-// }
-
 function Project( {image, description } ) {
   return (
     <div className="project">
       <img className="project-image" src={image} alt="project image" />
       <div className="information">
-        <p>{description}</p>
+        <div className="project-title"></div>
+        <div className="description">{description}</div>
+        <div className="project-links"></div>
+        <div className="project-skills-container">
+          <div className="project-skills">
+            
+          </div>
+        </div>
       </div>
     </div>
   )
@@ -39,6 +28,7 @@ export default function Projects() {
     <div className="projects-section">
       <SectionHeader title="Projects"/>
       <div className="projects-container">
+        <Project image={etchProjectImage} description=""/>
         <Project image={etchProjectImage} description="Hi there"/>
       </div>
     </div>
