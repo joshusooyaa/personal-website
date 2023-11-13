c42d003911636f6aa80c5869ffb137e381c9c58c
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-12 16:30:50 -0800

    Finish styling for section headers

--- src/components/section-header.jsx
+++ src/components/section-header.jsx
@@ -4,7 +4,9 @@ export default function SectionHeader( {title} ) {
 
   return (
     <>
-      <h3 className="section-header">{title}</h3>
+      <div className="section-header">
+        <h3 className='title'>{title}</h3>
+      </div>
     </>
   )
 }
