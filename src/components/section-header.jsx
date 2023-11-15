a7824d0c48721768db12a5aaea2b23f032550cb4
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-15 13:16:07 -0800

    Add IntersectionObserver for section headers

--- src/components/section-header.jsx
+++ src/components/section-header.jsx
@@ -1,11 +1,40 @@
+import { useEffect, useRef } from 'react'
 import '../styles/section-header.css'
 
 export default function SectionHeader( {title} ) {
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active-section');
+          }
+          else {
+            entry.target.classList.remove('active-section');
+          }
+        });
+      },
+      { threshold: .1 }
+    );
+
+    const element = sectionRef.current;
+    if (element) {
+      observer.observe(element);
+    }
+
+    return () => {
+      if (element) {
+        observer.unobserve(element);
+      }
+    };
+  }, []);
 
   return (
     <>
       <div className="section-header">
-        <h3 className='title'>{title}</h3>
+        <h3 ref={sectionRef} className='title'>{title}</h3>
       </div>
     </>
   )
