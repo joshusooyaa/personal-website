5695da10f73165884149cbd50f0ab5d7980e2cd1
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-11-16 14:10:28 -0800

    Add more to about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,6 @@
 import SectionHeader from './section-header'
-import Skills from './skills'          
+import Skills from './skills' 
+import { Link } from 'react-router-dom'         
 
 import '../styles/about.css'
 
@@ -7,18 +8,23 @@ export default function About() {
   return (
     <div name="about" className="about-section">
       <SectionHeader title="About" />
-      <div className="about-container">
-        <div className="salutation">
-          <h2>
-            Hey there!👋
-          </h2>
-          <h2 className="name">
-            I'm Josh
-          </h2>
+      <div className="about-area">
+        <div className="about-container">
+          <div className="salutation">
+            <h2>
+              Hey there!👋
+            </h2>
+            <h2 className="name">
+              I'm Josh
+            </h2>
+          </div>
+          <div className="abouts">
+            <p className="section intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p className="section interests">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit </p>
+          </div>
         </div>
-        <div className="abouts">
-          <p className="section intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-          <p className="section interests">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit </p>
+        <div className="more-about-container">
+          <Link to="about" className="more-about-link">Get to know me more<span>➔</span></Link>
         </div>
       </div>
       <div className="skills-container">
