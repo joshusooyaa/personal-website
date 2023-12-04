3a66614be095edf2027303f1237eeda747a3d26c
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-12-04 15:09:47 -0800

    Add header to about page

--- src/components/more-about.jsx
+++ src/components/more-about.jsx
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom'
 
+import githubLogo from '../assets/github-mark-white.svg'
+
+import '../styles/more-about.css'
+
+
 export default function MoreAbout() {
   return (
-    <>
-      <div>
+    <div className="more-about-page">
+      <div className="header blurred">
+        <div className="contact-fields visible">
+          <div className="name">Josh Sawyer</div>
+          <a className="github" href="https://github.com/joshusooyaa"><img src={githubLogo} alt="Link to Github" /></a>
+        </div>
+        <div className="navigation-fields">
+          <Link className="back-button" to="/">Back</Link>
+        </div>
+      </div>
+      <div className="about-container">
         Coming soon
       </div>
-      <button><Link to="/" className="go-back">Go Back</Link></button>
-    </>
+    </div>
   )
 }
