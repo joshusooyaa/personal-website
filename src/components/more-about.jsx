17596e3baca4f81787766911d508adbddbe3499d
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2023-12-04 15:46:17 -0800

    Update grid layout

--- src/components/more-about.jsx
+++ src/components/more-about.jsx
@@ -1,14 +1,36 @@
 import { Link } from 'react-router-dom'
 
+import debuggingImg from '../assets/about-images/debugging.jpg'
+import teachingImg from '../assets/about-images/teaching.jpg'
 import githubLogo from '../assets/github-mark-white.svg'
 
 import '../styles/more-about.css'
 
-function Section() {
+
+const aboutData = [
+  {
+    text: "When I first stepped into the door of software development as a student, I quickly found out that with coding comes bugs, lots of them. As a result, I've been constantly developing my ability to troubleshoot and resolve code that isn't working. Learning both C and C++ has helped me tremendously while working with memory, and ultimately it helped my overall understanding of how code is working in the first place. I remember a time my freshman year when I felt completely lost working with C++, struggling to understand how pointers work, but now it's one of my favorite languages to work with. I'm excited to see what projects and problems I can work on as I start my career as a software developer.",
+    image: debuggingImg
+  },
+  {
+    text: "As someone who loves learning, I'm always happy to help people learn new things and overcome their challenges. This is one of the reasons why I enjoy Computer Science so much, because not only do I get to challenge myself but I also get to help others who are struggling with what I've been able to learn. In one of my more complicated courses, I was able to lead a study group where I got to teach concepts to my peers to help them understand the course material. This opportunity has only strengthened my desire to continue in this field, and I'm looking forward to being able to contribute to projects, developing these skills, and helping others grow in the process.",
+    image: teachingImg
+  },
+  {
+    text: "When I first stepped into the door of software development as a student, I quickly found out that with coding comes bugs, lots of them. As a result, I've been constantly developing my ability to troubleshoot and resolve code that isn't working. Learning both C and C++ has helped me tremendously while working with memory, and ultimately it helped my overall understanding of how code is working in the first place. I remember a time my freshman year when I felt completely lost working with C++, struggling to understand how pointers work, but now it's one of my favorite languages to work with. I'm excited to see what projects and problems I can work on as I start my career as a software developer.",
+    image: debuggingImg
+  },
+  {
+    text: "As someone who loves learning, I'm always happy to help people learn new things and overcome their challenges. This is one of the reasons why I enjoy Computer Science so much, because not only do I get to challenge myself but I also get to help others who are struggling with what I've been able to learn. In one of my more complicated courses, I was able to lead a study group where I got to teach concepts to my peers to help them understand the course material. This opportunity has only strengthened my desire to continue in this field, and I'm looking forward to being able to contribute to projects, developing these skills, and helping others grow in the process.",
+    image: teachingImg
+  }
+]
+
+function Section( {text, image} ) {
   return (
     <div className="info-section">
-      <p className="text-area">This is where I will talk about myself</p>
-      <img src={githubLogo} alt="Img here" />
+      <p className="text-area">{text}</p>
+      <img src={image} alt="Img here" />
     </div>
   )
 }
@@ -26,8 +48,9 @@ export default function MoreAbout() {
         </div>
       </div>
       <div className="about-container">
-        <Section/>
-        <Section/>
+        {aboutData.map((sectionInfo, index) => (
+          <Section key={index} text={sectionInfo.text} image={sectionInfo.image}/>
+        ))}
       </div>
     </div>
   )
